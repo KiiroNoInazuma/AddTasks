affb35e9ee48434c11832b27d9eeb3196266db61
John Rey <adioskid@mail.ru>
2023-01-27 22:55:09 +0400

    Additional tasks

---
+++ src/CountLines.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class CountLines {
+    private static final Scanner sc = new Scanner(System.in);
+
+    CountLines(String text) {
+        numberOfRepeatedWords(text);
+    }
+
+    private void numberOfRepeatedWords(String text) {
+        List<String> list = List.of(text.toUpperCase().split(" "));
+        Map<String, Integer> map = new HashMap<>();
+        for (String s : list) {
+            if (map.containsKey(s)) {
+                map.put(s, map.get(s) + 1);
+            } else {
+                map.put(s, 1);
+            }
+        }
+        for (Map.Entry<String, Integer> next : map.entrySet()) {
+            System.out.println(next.getKey() + " : " + next.getValue());
+        }
+    }
+
+    public static void main(String[] args) {
+        new CountLines(sc.nextLine());
+
+    }
+}
+
+
+
+
+
